86708f76bbf6aba9619a98a05ca45bd601895cef
Tushar Gajbhiye <tushargajbhiye2003@gmail.com>
2026-02-01 19:00:42 +0530

    Fix: Add 0.0.0.0 for Render port binding

--- server.js
+++ server.js
@@ -50,4 +50,6 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, "0.0.0.0", () =>
+  console.log(`Server running on port ${port}`)
+);
